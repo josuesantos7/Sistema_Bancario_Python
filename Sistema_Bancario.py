dedc27238c6bf7bd3da7da7838cd0ccf86371364
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-05-21 12:11:23 -0300

    adicionado a função Extrato.

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -56,7 +56,10 @@ while True:
             print("Operação falhou! O valor informado é inválido.")
     
     elif opcao == "e":
-        print("Extrato")
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
 
     elif opcao == "q":
         print("Saindo...")
